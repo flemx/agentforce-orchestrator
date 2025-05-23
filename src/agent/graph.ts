b320febd41b116942379f65ba7b5e0aed283ceb1
flemx <damien@fleminks.com>
2025-05-23 12:17:06 +0200

    updates

--- src/agent/graph.ts
+++ src/agent/graph.ts
@@ -1,13 +1,13 @@
 import type { AIMessage } from "@langchain/core/messages";
-import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
+// import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
 import { ChatOpenAI } from "@langchain/openai";
 
 import { MessagesAnnotation, StateGraph } from "@langchain/langgraph";
-import { ToolNode } from "@langchain/langgraph/prebuilt";
+// import { ToolNode } from "@langchain/langgraph/prebuilt";
 
-const tools = [
-  new TavilySearchResults({ maxResults: 3, }),
-];
+// const tools = [
+//   new TavilySearchResults({ maxResults: 3, }),
+// ];
 
 // Define the function that calls the model
 async function callModel(
@@ -19,7 +19,7 @@ async function callModel(
    */
   const model = new ChatOpenAI({
     model: "gpt-4o",
-  }).bindTools(tools);
+  });
 
   const response = await model.invoke([
     {
@@ -38,9 +38,9 @@ function routeModelOutput(state: typeof MessagesAnnotation.State) {
   const messages = state.messages;
   const lastMessage: AIMessage = messages[messages.length - 1];
   // If the LLM is invoking tools, route there.
-  if ((lastMessage?.tool_calls?.length ?? 0) > 0) {
-    return "tools";
-  }
+  // if ((lastMessage?.tool_calls?.length ?? 0) > 0) {
+  //   return "tools";
+  // }
   // Otherwise end the graph.
   return "__end__";
 }
@@ -51,26 +51,26 @@ function routeModelOutput(state: typeof MessagesAnnotation.State) {
 const workflow = new StateGraph(MessagesAnnotation)
   // Define the two nodes we will cycle between
   .addNode("callModel", callModel)
-  .addNode("tools", new ToolNode(tools))
+  // .addNode("tools", new ToolNode(tools))
   // Set the entrypoint as `callModel`
   // This means that this node is the first one called
   .addEdge("__start__", "callModel")
-  .addConditionalEdges(
-    // First, we define the edges' source node. We use `callModel`.
-    // This means these are the edges taken after the `callModel` node is called.
-    "callModel",
-    // Next, we pass in the function that will determine the sink node(s), which
-    // will be called after the source node is called.
-    routeModelOutput,
-    // List of the possible destinations the conditional edge can route to.
-    // Required for conditional edges to properly render the graph in Studio
-    [
-      "tools",
-      "__end__"
-    ],
-  )
+  // .addConditionalEdges(
+  //   // First, we define the edges' source node. We use `callModel`.
+  //   // This means these are the edges taken after the `callModel` node is called.
+  //   "callModel",
+  //   // Next, we pass in the function that will determine the sink node(s), which
+  //   // will be called after the source node is called.
+  //   routeModelOutput,
+  //   // List of the possible destinations the conditional edge can route to.
+  //   // Required for conditional edges to properly render the graph in Studio
+  //   [
+  //     "tools",
+  //     "__end__"
+  //   ],
+  // )
   // This means that after `tools` is called, `callModel` node is called next.
-  .addEdge("tools", "callModel");
+  // .addEdge("tools", "callModel");
 
 // Finally, we compile it!
 // This compiles it into a graph you can invoke and deploy.
